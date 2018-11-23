66884b06207d6a078d4a44657804a97c1dacab1f
unknown <patelgaurav1202@gmail.com>
2018-11-23 22:00:32 +0530

    Cal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InvoiceComponent } from './invoice/invoice.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ContenteditableModule } from 'ng-contenteditable';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    InvoiceComponent
+    InvoiceComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ContenteditableModule,
+    // Import one or both of this modules
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice',
@@ -8,20 +9,65 @@ import { Component, OnInit } from '@angular/core';
 export class InvoiceComponent implements OnInit {
   item:any;
   itemObject:Object;
-  constructor() { }
+  dataForm:Object;
+  myControl = new FormControl;
+  constructor() {
+
+   }
 
   ngOnInit() {
-  	this.item=[]
-  	this.itemObject={
-  		"Item":'Name of Item',
-  		"Rate":10,
-  		"Quantity":10,
-  	}
+  	this.dataForm={
+  	"Invoice":0,
+  	"date":'',
+  	"Duedate":'',
+  	"subTotal":0,
+  	"texRate":0,
+  	"totalAmount":0,
+  	"amountPaid":0,
+  	"balanceDue":0,
+  }
+  this.item=[]
+  
 
   }
   addItem(){
-  	this.item.push(this.itemObject)
+  	let listObject:any={
+  		'id': this.item.length,
+  		"Item":'Name of Item',
+  		"Rate":"0",
+  		"Quantity":"1",
+  	}
+  	this.item.push(listObject)
 
+  	this.InvoiceCulc()
+   console.log(this.item)
+  }
+  removeItem(data){
+  	this.item.splice(data, 1)
+  	this.InvoiceCulc()
+
+  }
+  InvoiceCulc(){
+  	for (var i = this.item.length - 1; i >= 0; i--) {
+  		this.item[i].Rate =Number(this.item[i].Rate)
+  		this.item[i].Quantity =Number(this.item[i].Quantity)
+  	}
+  	if(this.item.length>1){
+  	this.dataForm.subTotal= this.item.reduce(function(previousValue, currentValue) {
+  			return  previousValue+  currentValue.Rate* currentValue.Quantity;
+  },0);
+  	
+  }else if(this.item.length==1){
+     this.dataForm.subTotal=this.item[0].Rate*this.item[0].Quantity
+    
+  }
+  this.texAmount=this.dataForm.subTotal*(this.dataForm.texRate/100)
+  this.dataForm.totalAmount=this.dataForm.subTotal+this.texAmount
+  this.dataForm.balanceDue=this.dataForm.totalAmount-this.dataForm.amountPaid
+
+  }
+  printInvoice(){
+  	window.print()
   }
 
 /* Prototyping
